3ea4b43013bdada4d638ec390f487071c36aaf5a
MarisV <maris.vigulis@protonmail.com>
2024-05-14 22:15:07 +0300

    some refactoring

--- src/Controller/TestsController.php
+++ src/Controller/TestsController.php
@@ -3,18 +3,16 @@
 namespace App\Controller;
 
 use App\Repository\UserOAuthRepository;
-use App\Service\Enums\Providers;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\DependencyInjection\ParameterBag\ParameterBagInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class TestsController extends AbstractController
 {
     #[Route('/tests', name: 'app_tests')]
-    public function index(UserOAuthRepository $repository): Response
+    public function index(ParameterBagInterface $bag): Response
     {
-
-        $auth = $repository->findForTokenRefresh(Providers::SPOTIFY);
 
         return $this->render('tests/index.html.twig', [
             'controller_name' => 'TestsController',
