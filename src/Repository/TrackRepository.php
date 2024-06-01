5dece6fa3528c0af75a775ed0bb34f20bf5d5f08
MarisV <maris.vigulis@protonmail.com>
2024-06-01 22:44:00 +0300

    cleanup

--- src/Repository/TrackRepository.php
+++ src/Repository/TrackRepository.php
@@ -21,30 +21,6 @@ class TrackRepository extends ServiceEntityRepository
         parent::__construct($registry, Track::class);
     }
 
-//    /**
-//     * @return Track[] Returns an array of Track objects
-//     */
-//    public function findByExampleField($value): array
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->orderBy('t.id', 'ASC')
-//            ->setMaxResults(10)
-//            ->getQuery()
-//            ->getResult()
-//        ;
-//    }
-
-//    public function findOneBySomeField($value): ?Track
-//    {
-//        return $this->createQueryBuilder('t')
-//            ->andWhere('t.exampleField = :val')
-//            ->setParameter('val', $value)
-//            ->getQuery()
-//            ->getOneOrNullResult()
-//        ;
-//    }
     public function getIdentifiers()
     {
         return $this->createQueryBuilder('t')
